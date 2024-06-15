c97f28875f560d7c0db5f6e77478cb09db96487c
Ahmad Syauqi <syauqi0291@gmail.com>
2024-06-15 22:26:01 +0700

    fixing row importing

--- app/Imports/DataImports.php
+++ app/Imports/DataImports.php
@@ -4,14 +4,20 @@ namespace App\Imports;
 
 use App\Models\Data;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class DataImports implements ToModel
+class DataImports implements ToModel, WithStartRow
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+
+    public function startRow(): int
+    {
+        return 2;
+    }
     public function model(array $row)
     {
         return new Data([
