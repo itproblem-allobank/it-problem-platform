c74e6f32e1fed8f64d401be540c67de1c0a2f4e2
Ahmad Syauqi <ahmad.syauqi@allobank.com>
2025-08-21 13:42:26 +0700

    update blocked by DLP, takeout Description

--- app/Imports/DataImports.php
+++ app/Imports/DataImports.php
@@ -31,28 +31,22 @@ class DataImports implements ToModel, WithStartRow, WithMultipleSheets
     }
     public function model(array $row)
     {
-        // convert hyperlink to code jira
-        // $hyperlink = $row[0]; // Gunakan regular expression untuk mendapatkan kode Jira
-        // preg_match('/browse\/([A-Z]+-\d+)/', $hyperlink, $matches);
 
-        // // $matches[1] akan berisi 'DIPM-3681'
-        // $code_jira = $matches[1];
-
+        $row16 = ($row[16] - 25569) * 86400;
+        $created = gmdate("Y-m-d H:i:s", $row16);
         $row17 = ($row[17] - 25569) * 86400;
-        $created = gmdate("Y-m-d H:i:s", $row17);
+        $updated = gmdate("Y-m-d H:i:s", $row17);
         $row18 = ($row[18] - 25569) * 86400;
-        $updated = gmdate("Y-m-d H:i:s", $row18);
-        $row19 = ($row[19] - 25569) * 86400;
-        $changed = gmdate("Y-m-d H:i:s", $row19);
+        $changed = gmdate("Y-m-d H:i:s", $row18);
 
         // set RCA Time
         $rca_fix = null;
-        if ($row[20] == null) {
+        if ($row[19] == null) {
             $rca_time = null;
             $rca_days = null;
         } else {
-            $row20 = ($row[20] - 25569) * 86400;
-            $rca_time = gmdate("Y-m-d H:i:s", $row20);
+            $row19 = ($row[19] - 25569) * 86400;
+            $rca_time = gmdate("Y-m-d H:i:s", $row19);
             $rca_days = Carbon::parse($created)->diffInDays(Carbon::parse($rca_time));
             // dd($rca_days);
             if ($rca_days <= 1) {
@@ -63,23 +57,23 @@ class DataImports implements ToModel, WithStartRow, WithMultipleSheets
         }
 
         // set Closed Time
-        if ($row[21] == null) {
+        if ($row[20] == null) {
             $closed_time = null;
         } else {
-            $row21 = ($row[21] - 25569) * 86400;
-            $closed_time = gmdate("Y-m-d H:i:s", $row21);
+            $row20 = ($row[20] - 25569) * 86400;
+            $closed_time = gmdate("Y-m-d H:i:s", $row20);
         }
 
         // set Target Date
-        if ($row[25] == null) {
+        if ($row[24] == null) {
             $target_date = null;
         } else {
-            $row25 = ($row[25] - 25569) * 86400;
-            $target_date = gmdate("Y-m-d H:i:s", $row25);
+            $row24 = ($row[24] - 25569) * 86400;
+            $target_date = gmdate("Y-m-d H:i:s", $row24);
         }
 
         // pisahkan problem & category 
-        $problem_category = $row[2];
+        $problem_category = $row[3];
         $array_problem_cat = explode(" - ", $problem_category);
         $problem = $array_problem_cat[0];
         $category = $array_problem_cat[1] ?? "-";
@@ -93,10 +87,10 @@ class DataImports implements ToModel, WithStartRow, WithMultipleSheets
 
         $data = [
             'code_jira'         => $row[0],
-            'environment'       => $row[1],
+            'summary'           => $row[1],
+            'environment'       => $row[2],
             'problem'           => $problem,
             'category'          => $category,
-            'summary'           => $row[3],
             'zentao_link'       => $row[4],
             'priority'          => $row[5],
             'status'            => $row[6],
@@ -107,9 +101,8 @@ class DataImports implements ToModel, WithStartRow, WithMultipleSheets
             'work_around'       => $row[11],
             'reporter'          => $row[12],
             'assignee_to'       => $row[13],
-            'description'       => $row[14],
-            'frequent'          => $row[15],
-            'complain_info'     => $row[16],
+            'frequent'          => $row[14],
+            'complain_info'     => $row[15],
             'created'           => $created,
             'updated'           => $updated,
             'changed_at'        => $changed,
@@ -117,9 +110,9 @@ class DataImports implements ToModel, WithStartRow, WithMultipleSheets
             'closed_time'       => $closed_time,
             'resolved_days'     => $resolved_days,
             'rca_days'          => $rca_fix,
-            'team'              => $row[22],
-            'aspect'            => $row[23],
-            'zoho_ticket'       => $row[24],
+            'team'              => $row[21],
+            'aspect'            => $row[22],
+            'zoho_ticket'       => $row[23],
             'target_date'       => $target_date
         ];
 
@@ -128,17 +121,17 @@ class DataImports implements ToModel, WithStartRow, WithMultipleSheets
             $data = array_merge($data, [
                 'nickname' => 'Syauqi',
             ]);
-        } else if ($assignee_too == 'Daffa Ramadhan') {
+        } else if ($assignee_too == 'Fadel Ferniawan') {
             $data = array_merge($data, [
-                'nickname' => 'Daffa',
+                'nickname' => 'Fadel',
             ]);
         } else if ($assignee_too == 'Nanda Mahdiaritama Basuki') {
             $data = array_merge($data, [
                 'nickname' => 'Nanda',
             ]);
-        } else if ($assignee_too == 'Stefano Adrian Sambora') {
+        } else if ($assignee_too == 'Febri Syahri Ramadhan') {
             $data = array_merge($data, [
-                'nickname' => 'Stefano',
+                'nickname' => 'Febri',
             ]);
         } else if ($assignee_too == 'Ian Daniel Adinata') {
             $data = array_merge($data, [
@@ -151,10 +144,6 @@ class DataImports implements ToModel, WithStartRow, WithMultipleSheets
         } else if ($assignee_too == 'Tri Intan Siska Permatasari') {
             $data = array_merge($data, [
                 'nickname' => 'Intan',
-            ]);
-        } else if ($assignee_too == 'Alexander Lucas') {
-            $data = array_merge($data, [
-                'nickname' => 'Lucas',
             ]);
         } else if ($assignee_too == 'Fachri Fachri') {
             $data = array_merge($data, [
--- app/Models/Data.php
+++ app/Models/Data.php
@@ -9,5 +9,5 @@ class Data extends Model
 {
     protected $table = 'excel_data';
 
-    protected $fillable = ['code_jira', 'environment', 'problem', 'category', 'summary', 'zentao_link', 'priority', 'status', 'pending_reason', 'target_version', 'impact_analyst', 'root_cause', 'work_around', 'reporter', 'assignee_to', 'description', 'frequent', 'complain_info', 'created', 'updated', 'changed_at', 'nickname', 'rca_time', 'closed_time', 'resolved_days', 'rca_days', 'team', 'aspect', 'zoho_ticket', 'target_date'];
+    protected $fillable = ['code_jira', 'environment', 'problem', 'category', 'summary', 'zentao_link', 'priority', 'status', 'pending_reason', 'target_version', 'impact_analyst', 'root_cause', 'work_around', 'reporter', 'assignee_to', 'frequent', 'complain_info', 'created', 'updated', 'changed_at', 'nickname', 'rca_time', 'closed_time', 'resolved_days', 'rca_days', 'team', 'aspect', 'zoho_ticket', 'target_date'];
 }
